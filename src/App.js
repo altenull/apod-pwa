120f3e035ecf6d7b77496366968ba5cc5b0ecc55
altenull <altenull@gmail.com>
2018-03-01 01:11:55 +0900

    applied semantic-ui and Switch to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import { Home, Gallery } from 'pages';
+import AppShell from './AppShell';
+import 'semantic-ui-css/semantic.min.css';
 
+// 0. AppShell.. Header & Drawer
+// 1. contents.. Home  (+ Dimmer when network connection loss)
+// 2. contents.. Gallery
 class App extends Component {
   render() {
     return (
-      <div>
-        App
-      </div>
+      <React.Fragment>
+        <AppShell>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/gallery' component={Gallery} />
+          </Switch>
+        </AppShell>
+      </React.Fragment>
     );
   }
 }
