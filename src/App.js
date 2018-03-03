eda89fed613b907b2160a1443c0f8a303ac9e269
altenull <altenull@gmail.com>
2018-03-03 23:40:21 +0900

    find APOD in IndexedDB when Application started and set to store

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { GalleryActions } from 'store/actionCreators';
 import { Home, Gallery } from 'pages';
 import AppShell from './AppShell';
 import 'semantic-ui-css/semantic.min.css';
+import IndexedDB from 'lib/IndexedDB';
 
 // 0. AppShell.. Header & Drawer
 // 1. contents.. Home  (+ Dimmer when network connection loss)
 // 2. contents.. Gallery
 class App extends Component {
+  componentWillMount() {
+    IndexedDB.init().then(() => {
+      IndexedDB.getAllAPODs().then((findResult) => {
+        GalleryActions.setAPODList(findResult);
+      });
+    });
+  }
+
+  componentWillUnmount() {
+    IndexedDB.close();
+  }
+
   render() {
     return (
       <React.Fragment>
