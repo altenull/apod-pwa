d7d504b533d0d8d0f742fdc6607a9d11d26cbe0c
altenull <altenull@gmail.com>
2018-03-04 23:50:34 +0900

    implemented DimmerContainer

---
+++ src/containers/common/DimmerContainer.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import type { State } from 'store';
+import { BaseActions } from 'store/actionCreators';
+import Dimmer from 'components/common/Dimmer';
+
+type Props = {
+  drawer: boolean
+}
+
+class DimmerContainer extends Component<Props> {
+  handleClick = () => {
+    BaseActions.hideDrawer();
+  }
+
+  render() {
+    const { drawer } = this.props;
+    const { handleClick } = this;
+
+    if (!drawer) {
+      return null;
+    }
+
+    return (
+      <Dimmer
+        onClick={handleClick}
+      />
+    );
+  }
+}
+
+export default connect(
+  ({ base }: State) => ({
+    drawer: base.drawer
+  }),
+  () => ({})
+)(DimmerContainer);
