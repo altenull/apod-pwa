f0a0af221d32c58649db2347d2b952c113dde0a9
altenull <altenull@gmail.com>
2018-03-06 21:35:04 +0900

    added Drawer hide function to HomeTemplateContainer

--- src/containers/home/HomeTemplateContainer.js
+++ src/containers/home/HomeTemplateContainer.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
+import { BaseActions } from 'store/actionCreators';
 import HomeTemplate from 'components/home/HomeTemplate';
 import CalendarNavigatorContainer from 'containers/home/CalendarNavigatorContainer';
-import ViewerContainer from 'containers/home/ViewerContainer';
+import ViewerTemplateContainer from 'containers/home/ViewerTemplateContainer';
 
 class HomeTemplateContainer extends Component {
+  handleClick = () => BaseActions.hideDrawer()
+
   render() {
-    const viewer = <ViewerContainer />;
+    const { handleClick } = this;
+    const viewer = <ViewerTemplateContainer />;
     const dateNavigator = <CalendarNavigatorContainer />;
 
     return (
       <HomeTemplate
         viewer={viewer}
         dateNavigator={dateNavigator}
+        onClick={handleClick}
       />
     );
   }
