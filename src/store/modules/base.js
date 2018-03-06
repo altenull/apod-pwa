5592420fd2aff88b96ac113fc67348da1566742c
altenull <altenull@gmail.com>
2018-03-06 21:36:36 +0900

    updated base module

--- src/store/modules/base.js
+++ src/store/modules/base.js
@@ -5,33 +5,35 @@ import { Record, type Map } from 'immutable';
 const INITIALIZE_BASE = 'base/INITIALIZE_BASE';
 const SHOW_DRAWER = 'base/SHOW_DRAWER';
 const HIDE_DRAWER = 'base/HIDE_DRAWER';
-const OPEN_MODAL = 'base/OPEN_MODAL';
-const CLOSE_MODAL = 'base/CLOSE_MODAL';
+const OPEN_APOD_MODAL = 'base/OPEN_APOD_MODAL';
+const CLOSE_APOD_MODAL = 'base/CLOSE_APOD_MODAL';
 
 export type BaseActionCreators = {
   initializeBase(): any,
   showDrawer(): any,
   hideDrawer(): any,
-  openModal(): any,
-  closeModal(): any
+  openAPODModal(date: ?string): any,
+  closeAPODModal(): any
 };
 
 export const actionCreators = {
   initializeBase: createAction(INITIALIZE_BASE),
   showDrawer: createAction(SHOW_DRAWER),
   hideDrawer: createAction(HIDE_DRAWER),
-  openModal: createAction(OPEN_MODAL),
-  closeModal: createAction(CLOSE_MODAL)
+  openAPODModal: createAction(OPEN_APOD_MODAL),
+  closeAPODModal: createAction(CLOSE_APOD_MODAL)
 };
 
 export type Base = {
   drawer: boolean,
-  modal: boolean
+  apodModal: boolean,
+  apodDate: ?string
 };
 
 const BaseRecord = Record({
   drawer: false,
-  modal: false
+  apodModal: false,
+  apodDate: null
 });
 
 const initialState: Map<string, *> = BaseRecord();
@@ -40,6 +42,12 @@ export default handleActions({
   [INITIALIZE_BASE]: state => initialState,
   [SHOW_DRAWER]: state => state.set('drawer', true),
   [HIDE_DRAWER]: state => state.set('drawer', false),
-  [OPEN_MODAL]: state => state.set('modal', true),
-  [CLOSE_MODAL]: state => state.set('modal', false)
+  [OPEN_APOD_MODAL]: (state, { payload: date }) => {
+    if (!date) {
+      return state.set('apodModal', true);
+    } else {
+      return state.set('apodModal', true).set('apodDate', date);
+    }
+  },
+  [CLOSE_APOD_MODAL]: state => state.set('apodModal', false).set('apodDate', null)
 }, initialState);
