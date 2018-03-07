9b0d98ddb3b20d919a0d275d51b701b47a10f56e
altenull <altenull@gmail.com>
2018-03-07 16:44:02 +0900

    applied Google Analystic

--- src/Root.js
+++ src/Root.js
@@ -3,6 +3,9 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import store from './store';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_ID);
 
 const Root = () => (
   <Provider store={store}>
