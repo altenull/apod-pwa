f287e70927dce3a976e7762f4979b4e981178882
altenull <altenull@gmail.com>
2018-03-07 12:54:41 +0900

    changed componentWillUnmount method in pages

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -2,11 +2,9 @@ import React, { Component } from 'react';
 import { BaseActions } from 'store/actionCreators';
 import GalleryTemplateContainer from 'containers/gallery/GalleryTemplateContainer';
 
-// Masonry Layout 사용.
 class Gallery extends Component {
   componentWillUnmount() {
-    BaseActions.initializeBase();
-    
+    BaseActions.hideDrawer();
   }
 
   render() {
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,10 @@
 import React, { Component } from 'react';
-import { BaseActions, HomeActions } from 'store/actionCreators';
+import { BaseActions } from 'store/actionCreators';
 import HomeTemplateContainer from 'containers/home/HomeTemplateContainer';
-// TODO
-// 네트워크 상태 Dimmer
-// 네비게이션 왼쪽, 오른쪽
-// 날짜 선택
-// Save to My Gallery 기능.
-// 
+
 class Home extends Component {
   componentWillUnmount() {
-    BaseActions.initializeBase();
-    HomeActions.initializeHome();
+    BaseActions.hideDrawer();
   }
 
   render() {
